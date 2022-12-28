c54a3293200da669ce059adf77abe0c741689cfc
Nick Sinnikov <nickosinnikov@outlook.com>
2022-12-28 17:39:40 +0300

    first commit

---
+++ src/components/NewsBlock.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default NewsBlock=(props)=>{
+return (
+    <NewsBlockHeader>
+    <ul>
+      <li>Сейчас в СМИ</li>
+      <li>В Германии</li>
+      <li>Остальной мир</li>
+    </ul>
+  </NewsBlockHeader>
+)
+}
